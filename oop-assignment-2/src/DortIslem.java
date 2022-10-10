cd66d3b32372bd0fcf2c0e3115a3a69de1d546f6
ASaidOguz <ahmetsaid2136@gmail.com>
2022-10-10 17:19:37 +0300

    initial commit

---
+++ oop-assignment-2/src/DortIslem.java
@@ -0,0 +1,9 @@
+public class DortIslem {
+    //Method overloading;
+    public int Topla(int sayı1,int sayı2){
+        return sayı1+sayı2;
+    }
+    public int Topla(int sayı1,int sayı2,int sayı3){
+        return sayı1+sayı2+sayı3;
+    }
+}
