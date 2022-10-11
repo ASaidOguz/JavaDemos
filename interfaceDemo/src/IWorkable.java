c90269d331e49003505f8aba3abe2d130a91786f
ASaidOguz <ahmetsaid2136@gmail.com>
2022-10-11 17:16:14 +0300

    initial commit

---
+++ interfaceDemo/src/IWorkable.java
@@ -0,0 +1,5 @@
+public interface IWorkable {
+    void work();
+    //Başka operasyonlar;
+    //Yani ortak operasyonları buraya yerleştireceğiz...
+}
---
+++ interfaceDemo/src/RobotWorker.java
@@ -0,0 +1,11 @@
+public class RobotWorker implements IWorkable,IMaintable{
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void maint() {
+
+    }
+}
